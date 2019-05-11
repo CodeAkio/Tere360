866dda2d63fbe4a8fc73eeb0e67bdd11807d6e13
CodeN2 <siqueira_2004@hotmail.com>
2019-05-10 22:34:24 -0300

    Fix mongodb connection

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 let app = require('./config/express')();
-require('./config/database')('mongodb://<admin>:<JJ8G338HfG6Ug8x>@ds155076.mlab.com:55076/tere360');
+require('./config/database')('mongodb://admin:JJ8G338HfG6Ug8x@ds155076.mlab.com:55076/tere360');
 
 app.listen(app.get('port'), () => {
     console.log(`Express Server on port ${app.get('port')}`);
